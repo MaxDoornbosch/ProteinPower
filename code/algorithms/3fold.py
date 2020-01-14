8b4161dd47d2c51470779a4b38599283f3637544
Max <max.doornbosch@gmail.com>
2020-01-14 14:55:52 +0100

    start of checking coordinates

--- code/algorithms/3fold.py
+++ code/algorithms/3fold.py
@@ -9,6 +9,9 @@ class Three_Fold():
         """
         Defines all possible folds for every move
         """
+        self.amino = amino
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
         if current_fold == 1:
             self.possible_folds.append([1, -2, 2])
         elif current_fold == -1:
@@ -53,8 +56,8 @@ class Three_Fold():
         Takes possible folds and attaches corresponding coordinates
         """
         for i in self.possible_three_folds:
-            self.current_x = x_coordinate
-            self.current_y = y_coordinate
+            self.current_x = self.x_coordinate
+            self.current_y = self.y_coordinate
             self.storage_list = []
 
             for j in range(3):
@@ -67,14 +70,57 @@ class Three_Fold():
                 elif i[j] == -2:
                     self.current_y -= 1
                 if j == 2:
-                    self.storage_list.append([amino[j], 0, self.current_x, self.current_y])
+                    self.storage_list.append([self.amino[j], 0, self.current_x, self.current_y])
                 else:
-                    self.storage_list.append([amino[j], i[j + 1], self.current_x, self.current_y])
+                    self.storage_list.append([self.amino[j], i[j + 1], self.current_x, self.current_y])
             self.information_folds.append(self.storage_list)
         print(self.information_folds)
         print(len(self.information_folds))
-        # print(self.possible_three_folds)
+
+    def stability_score(self, final_placement):
+        self.possible_coordinates = []
+        self.final_placement = final_placement
+        print(self.information_folds)
+
+        for possible_folds in self.information_folds:
+            checker = True
+            score = 0
+
+            # checks and saves possible folds
+            for i in (self.final_placement):
+                
+                for pos in range(3):
+                    if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3]:
+                        print(i[2], i[3])
+                        print(possible_folds[pos][2], possible_folds[pos][3])
+                        checker = False
+
+                        # save coordinates for visualization
+                        if i[0] == "H" and possible_folds[pos][0]:
+                            score -= 1
+                            
+
+            if checker == True:
+                self.possible_coordinates.append([possible_folds[0], possible_folds[1], possible_folds[2], score])
+
+        print("/////////")
+        print(self.possible_coordinates)
+        print(len(self.possible_coordinates))
+        # if self.possible_coordinates == []:
+        #     return self.possible_coordinates
+        # self.random_amino = random.choice(self.possible_coordinates)
+        # self.current_fold = self.new_fold
+        # if len(user_input) - 1 == len(self.final_placement):
+            # self.random_amino = [user_input[len(self.final_placement)], 0, self.current_x, self.current_y]
+        for value in self.possible_coordinates:
+            if value[3] != 0:
+                print("-------------------------------------------------")
+                print(value)
+        return True
 
 if __name__ == "__main__":
     test = Three_Fold()
-    test.recursion_folds("PPH", 1, 1, 0)
+    test.define_folds("HHH", 2, 0, 2)
+    test.set_coordinates()
+    final_placement = [["H", 1, -1, 0], ["H", 2, 0, 0], ["H", 2, 0, 1]]
+    test.stability_score(final_placement)
