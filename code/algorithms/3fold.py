a0a2b8e4f389894721f55abfa78e4f40c1abc258
Max <max.doornbosch@gmail.com>
2020-01-15 11:42:14 +0100

    stability score 3 fold

--- code/algorithms/3fold.py
+++ code/algorithms/3fold.py
@@ -12,6 +12,8 @@ class Three_Fold():
         self.amino = amino
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
+
+        # Checks the previous fold
         if current_fold == 1:
             self.possible_folds.append([1, -2, 2])
         elif current_fold == -1:
@@ -21,7 +23,10 @@ class Three_Fold():
         elif current_fold == -2:
             self.possible_folds.append([-1, 1, -2])
 
+        # Loop over all the possible folds
         for i in (self.possible_folds[0]):
+
+            # Checks which folds are possible
             if i == 1:
                 self.possible_folds.append([1, -2, 2])
             elif i == -1:
@@ -31,8 +36,10 @@ class Three_Fold():
             elif i == -2:
                 self.possible_folds.append([-1, 1, -2])
 
+            # Loops over all the previous possible folds
             for j in (self.possible_folds[1]):
-                print(j, i)
+
+                # Adds the folds to a list
                 if j == 1:
                     self.possible_three_folds.append([i, j, 1])
                     self.possible_three_folds.append([i, j, -2])
@@ -51,16 +58,22 @@ class Three_Fold():
                     self.possible_three_folds.append([i, j, -1])
             self.possible_folds.pop(1)
 
-    def set_coordinates(self):
+    def set_coordinates(self, current_amino):
         """
         Takes possible folds and attaches corresponding coordinates
         """
+
+        # Loops over all the possible three folds
         for i in self.possible_three_folds:
             self.current_x = self.x_coordinate
             self.current_y = self.y_coordinate
             self.storage_list = []
+            self.storage_list.append([current_amino, i[0], self.x_coordinate, self.y_coordinate])
 
+            # Loops over the folds separately
             for j in range(3):
+
+                # Changes the coordinates
                 if i[j] == 1:
                     self.current_x += 1
                 elif i[j] == -1:
@@ -69,35 +82,87 @@ class Three_Fold():
                     self.current_y += 1
                 elif i[j] == -2:
                     self.current_y -= 1
+
+                # Adds a dummy fold to the last coordinates
                 if j == 2:
                     self.storage_list.append([self.amino[j], 0, self.current_x, self.current_y])
                 else:
                     self.storage_list.append([self.amino[j], i[j + 1], self.current_x, self.current_y])
+
             self.information_folds.append(self.storage_list)
         print(self.information_folds)
         print(len(self.information_folds))
 
     def stability_score(self, final_placement):
+        """
+        Calculates the stability score for every three fold
+        """
+
         self.possible_coordinates = []
         self.final_placement = final_placement
         print(self.information_folds)
 
+        # Loops over all the three folds
         for possible_folds in self.information_folds:
             checker = True
             score = 0
 
-            # checks and saves possible folds
+            # Loops over all the places amino's
             for i in (self.final_placement):
                 
-                for pos in range(3):
+                # Loops over all the 
+                for pos in range(4):
                     if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3]:
                         print(i[2], i[3])
                         print(possible_folds[pos][2], possible_folds[pos][3])
                         checker = False
 
+                    if pos != 0 and checker == True:
+                        previous_fold = possible_folds[pos - 1][1]
+                        if previous_fold == -1:
+                            if i[2] == possible_folds[pos][2] - 1 and i[3] == possible_folds[pos][3]:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3] + 1:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3] - 1:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                                    
+                        if previous_fold == -2:
+                            if i[2] == possible_folds[pos][2] - 1 and i[3] == possible_folds[pos][3]:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] + 1 and i[3] == possible_folds[pos][3]:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3] - 1:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+
+                        if previous_fold == 1: 
+                            if i[2] == possible_folds[pos][2] + 1 and i[3] == possible_folds[pos][3]:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3] + 1:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3] - 1:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+
+                        if previous_fold == 2:
+                            if i[2] == possible_folds[pos][2] - 1 and i[3] == possible_folds[pos][3]:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] and i[3] == possible_folds[pos][3] + 1:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
+                            if i[2] == possible_folds[pos][2] + 1 and i[3] == possible_folds[pos][3]:
+                                if i[0] == "H" and possible_folds[pos][0]:
+                                    score -=1
                         # save coordinates for visualization
-                        if i[0] == "H" and possible_folds[pos][0]:
-                            score -= 1
                             
 
             if checker == True:
@@ -120,7 +185,7 @@ class Three_Fold():
 
 if __name__ == "__main__":
     test = Three_Fold()
-    test.define_folds("HHH", 2, 0, 2)
-    test.set_coordinates()
-    final_placement = [["H", 1, -1, 0], ["H", 2, 0, 0], ["H", 2, 0, 1]]
+    test.define_folds("HHH", 2, 0, 1)
+    test.set_coordinates("P")
+    final_placement = [["H", 2, 0, -1], ["H", 2, 0, 0]]
     test.stability_score(final_placement)
