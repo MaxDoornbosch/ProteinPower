de97a80ba01ef00cc494a694d2e4405621b47432
Max <max.doornbosch@gmail.com>
2020-01-17 16:37:40 +0100

    Start of the threefold 1 step

---
+++ code/algorithms/Threefoldonestep.py
@@ -0,0 +1,256 @@
+import random
+
+
+def three_fold(final_placement, user_input_split, current_fold, x_coordinate, y_coordinate, current_amino, i):
+    """
+    Determines best possible folds and coordinates for each amino (in each chunk)
+    """
+
+    storage_list = []
+    possible_options = []
+
+    chunk = user_input_split[i]
+    print(chunk)
+    possible_folds, final_possible_folds, possible_options = define_folds(current_fold, x_coordinate, y_coordinate, current_amino, possible_options, storage_list, chunk)
+    possible_options_score = stability_score(possible_options, final_placement, chunk)
+    best_option = best_options(possible_options_score)
+    return best_option
+
+
+def split_protein(user_input):
+    """
+    Splits user input protein sequence into chunks of (max) three amino acids while ignoring
+    the first amino as the first fold and coordinates are predefined.
+    """
+
+    user_input = user_input[2:]
+    user_input_split = [user_input[i:i+3] for i in range(0, len(user_input), 1)]
+    return user_input_split
+
+def define_folds(current_fold, x_coordinate, y_coordinate, current_amino, possible_options, storage_list, chunk):
+    """
+    Defines all possible folds for every move within
+    """
+    possible_folds = []
+    final_possible_folds = []
+    corresponding_folds = {
+       1 : [1, -2, 2],
+       -1 : [-1, -2, 2],
+       2 : [-1, 1, 2],
+       -2 : [-1, 1, -2]
+    }
+
+    # determines corresponding folds based on current fold
+    if current_fold in corresponding_folds.keys():
+        possible_folds.append(corresponding_folds.get(current_fold))
+
+    for i in (possible_folds[0]):
+        current_fold = i
+        if i in corresponding_folds.keys():
+            possible_folds.append(corresponding_folds.get(current_fold))
+
+        # saves every possible fold for each amino
+        for j in (possible_folds[1]):
+            current_fold = j
+            if j in corresponding_folds.keys():
+                final_possible_folds.append([i, j, corresponding_folds.get(current_fold)[0]])
+                final_possible_folds.append([i, j, corresponding_folds.get(current_fold)[1]])
+                final_possible_folds.append([i, j, corresponding_folds.get(current_fold)[2]])
+        print("possible folds", possible_folds)
+        possible_folds.pop(1)
+
+    possible_options = set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_amino, possible_options, chunk)
+    return possible_folds, final_possible_folds, possible_options
+
+def set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_amino, possible_options, chunk):
+    """
+    Takes possible folds and attaches corresponding coordinates
+    """
+
+    print("QQQQQQQQQQQQQQQ", chunk)
+
+    # saves all possible folds with corresponding amino and coordinates
+    for i in final_possible_folds:
+        current_x = x_coordinate
+        current_y = y_coordinate
+        storage_list = []
+        storage_list.append([current_amino, i[0], x_coordinate, y_coordinate])
+
+        # Simpeler maken !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (class???)
+        for j in range(len(chunk)):
+            if abs(i[j]) == 1:
+                current_x += i[j]
+            elif abs(i[j]) == 2:
+                current_y += i[j] // 2
+
+            # adds a dummy fold of 0 to the last coordinates
+            if j >= len(chunk) - 1:
+                storage_list.append([chunk[j], 0, current_x, current_y])
+            elif j < len(chunk) - 1:
+                storage_list.append([chunk[j], i[j + 1], current_x, current_y])
+
+            # prevents duplicates and saves aminos with corresponding information
+            if storage_list not in possible_options:
+                possible_options.append(storage_list)
+
+    print("Length of possible_options: ", len(possible_options))
+    print("All possible options (possible_options): ", possible_options)
+
+    return possible_options
+
+
+def stability_score(possible_options, final_placement, chunk):
+    """
+    Calculates the stability score for every three fold
+    """
+
+    possible_options_score = []
+
+
+    for unit in possible_options:
+        checker = True
+        fold = True
+        score = 0
+        temporary_amino_stability_x = []
+        temporary_amino_stability_y = []
+
+        # Loops over all the places aminos
+        for i in final_placement:
+
+            # Loops over all the coordinates and folds separately
+            for pos in range(len(chunk) + 1):
+
+                # Checks if the coordinates aren't already taken
+                if i[2] == unit[pos][2] and i[3] == unit[pos][3]:
+                    print(i[2], i[3])
+                    print(unit[pos][2], unit[pos][3])
+                    checker = False
+
+                # Checks if the coordinates overlap
+                if pos != 0 and checker == True:
+                    previous_fold = unit[pos - 1][1]
+
+                    # Looks for surrounding H aminos per fold and calculates the score
+                    if i[0] == "H" and unit[pos][0] == "H":
+                        if i[2] == unit[pos][2] - 1 and i[3] == unit[pos][3] and previous_fold != 1:
+                            score -=1
+                            if pos == 1:
+                                temporary_amino_stability_x.append([i[2], unit[pos][2]])
+                                temporary_amino_stability_y.append([i[3], unit[pos][3]])
+
+                        if i[2] == unit[pos][2] and i[3] == unit[pos][3] + 1 and previous_fold != -2:
+                            score -=1
+                            if pos == 1:
+                                temporary_amino_stability_x.append([i[2], unit[pos][2]])
+                                temporary_amino_stability_y.append([i[3], unit[pos][3]])
+
+                        if i[2] == unit[pos][2] and i[3] == unit[pos][3] - 1 and previous_fold != 2:
+                            score -=1
+                            if pos == 1:
+                                temporary_amino_stability_x.append([i[2], unit[pos][2]])
+                                temporary_amino_stability_y.append([i[3], unit[pos][3]])
+
+                        if i[2] == unit[pos][2] + 1 and i[3] == unit[pos][3] and previous_fold != -1:
+                            score -=1
+                            if pos == 1:
+                                temporary_amino_stability_x.append([i[2], unit[pos][2]])
+                                temporary_amino_stability_y.append([i[3], unit[pos][3]])
+
+                    # checks whether the last amino connects to itself
+                    if pos == 3 and fold == True:
+                        fold = False
+                        if unit[pos][0] == "H" and unit[0][0] == "H":
+                            if unit[pos][2] + 1 == unit[0][2] and unit[pos][3] == unit[0][3]:
+                                score -= 1
+
+                            if unit[pos][2] - 1 == unit[0][2] and unit[pos][3] == unit[0][3]:
+                                score -= 1
+
+                            if unit[pos][2] == unit[0][2] and unit[pos][3] + 1 == unit[0][3]:
+                                score -= 1
+
+                            if unit[pos][2] == unit[0][2] and unit[pos][3] - 1 == unit[0][3]:
+                                score -= 1
+
+        # saves all possible options with corresponding stability scores
+        if checker == True:
+            try:
+                possible_options_score.append([unit[0], unit[1], unit[2], score, temporary_amino_stability_x, temporary_amino_stability_y])
+
+            except IndexError:
+                possible_options_score.append([unit[0], unit[1], score, temporary_amino_stability_x, temporary_amino_stability_y])
+
+    print("////////////////")
+    print("Length of all possible options: ", len(possible_options_score))
+    print("All possible options with stability score: ", possible_options_score)
+
+    return possible_options_score
+
+    # if possible_options_score == []:
+    #     return possible_options_score
+    # random_amino = random.choice(possible_options_score)
+    # current_fold = new_fold
+    # if len(user_input) - 1 == len(final_placement):
+        # random_amino = [user_input[len(final_placement)], 0, current_x, current_y]
+
+def best_options(possible_options_score):
+    """
+    Prunes possible options with lowest values
+    """
+
+    lowest_stability_score = 0
+    best_options = []
+
+    # finds lower bound
+    for value in possible_options_score:
+        if value[len(value) - 3] < lowest_stability_score:
+            lowest_stability_score = value[len(value) - 3]
+            print("Lowest stability score is: ", lowest_stability_score)
+
+    # saves options with lower bound
+    for value in possible_options_score:
+        if value[len(value) - 3] == lowest_stability_score:
+            best_options.append(value)
+
+    print("Length of best options: ", len(best_options))
+    print("Best options: ", best_options)
+    best_option = choose_best_fold(best_options)
+    return best_option
+
+
+def choose_best_fold(best_options):
+    """
+    Chooses the most common fold
+    """
+    counter = 0
+    best_option = best_options[0][0][1]
+    best_folds = []
+    for option in best_options:
+        best_folds.append(option[0][1])
+    for option in best_folds:
+        current_frequency = best_folds.count(option)
+        if (current_frequency > counter):
+            counter = current_frequency 
+            best_option = option
+    print("The best fold is ", best)
+    best_option = choose_best_option(best_options)
+    return best_option
+
+def fold_with_stability(best_options, best_fold):
+    """
+    Adds the stability to the fold
+    """
+    for option in best_options:
+        if option[1] != best_fold:
+            best_options.remove(option)
+        
+    best_option_fold = random.choice(best_options)
+    best_option = []
+    best_option.append(best_option_fold[0][1])
+    best_option.append(best_option_fold[-2])
+    best_option.append(best_option_fold[-1])
+    return best_option
+
+            
+
+
--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from code.classes.csvwriter import Csv
 from code.visualization.visualization import visualize
 from code.algorithms.threefold import *
 
-
 if __name__ == "__main__":
     user_input = input("Please enter your protein (minimum length is 3): ").upper()
 
@@ -33,6 +32,7 @@ if __name__ == "__main__":
 
     # splits protein sequence into chunks of three amino acids
     user_input_split = split_protein(user_input)
+    print(user_input_split)
 
     # updates x,y coordinates based on most recently determined fold
     coordinate_update = CoordinateUpdate(protein.final_placement)
@@ -92,7 +92,88 @@ if __name__ == "__main__":
 
     print("check final placement", protein.final_placement)
 
+"""
+Threefold algorithm
 
+# if __name__ == "__main__":
+#     user_input = input("Please enter your protein (minimum length is 3): ").upper()
+
+#     # minimum length of protein
+#     while len(user_input) < 3:
+#         user_input = input("Please enter your protein (minimum length is 3): ").upper()
+
+#     # checks if user_input is valid
+#     for i in range(len(user_input)):
+#         while user_input[i] != "H" and user_input[i] != "P" and user_input[i] !="C":
+#             user_input = input("Please enter your protein (minimum length is 3): ").upper()
+
+#     # sets first fold and adds to final
+#     protein = Protein(user_input)
+
+
+#     # splits protein sequence into chunks of three amino acids
+#     user_input_split = split_protein(user_input)
+
+#     # updates x,y coordinates based on most recently determined fold
+#     coordinate_update = CoordinateUpdate(protein.final_placement)
+#     coordinate_update.update_coordinates(protein.final_placement)
+
+#     # quits updating once end of sequence has been reached
+#     if coordinate_update == False:
+#         exit()
+
+#     print(user_input_split[0])
+
+#     amino_stability_x = []
+#     amino_stability_y = []
+
+#     for i in range(len(user_input_split)):
+
+#         current_x, current_y  = coordinate_update.update_coordinates(protein.final_placement)
+
+#         print(len(protein.final_placement))
+#         print(len(user_input)-1)
+
+
+#         # extracts most recently added fold and amino and performs three fold algorithm
+#         current_fold = protein.final_placement[-1][1]
+
+#         # -1 want user input begint bij 0 en len van lijst niet
+#         current_amino = user_input[len(protein.final_placement)]
+
+#         print(user_input_split)
+
+#         best_option = three_fold(protein.final_placement, user_input_split, current_fold, current_x, current_y, current_amino, i)
+
+#         # ensures no empty lists are appended
+#         if best_option[-2]:
+#             amino_stability_x.extend(best_option[-2])
+#         if best_option[-1]:
+#             amino_stability_y.extend(best_option[-1])
+
+#         for i in range(len(best_option) - 3):
+#             protein.add_amino_info(best_option[i])
+
+#         if current_fold != 0:
+#             current_x, current_y = coordinate_update.update_coordinates(protein.final_placement)
+
+#         # checks if last amino of sequence is reached
+#         if len(protein.final_placement) == (len(user_input) - 1):
+#             protein.add_last_amino_of_chunk(current_x, current_y, user_input)
+
+#         if protein.final_placement[-1][1] == 0:
+#             print("End of protein ------------------->>>>> :) :) :) :)")
+#             stability = Stability()
+#             print(stability.score(protein.final_placement, user_input))
+#             csvwriter = Csv(protein.final_placement)
+#             csvwriter.write_csv()
+#             csvwriter.visualization_csv()
+#             visualize('data/visualization.csv', user_input, stability.definitive_stability_score, amino_stability_x, amino_stability_y)
+
+#     print("check final placement", protein.final_placement)
+"""
+
+####################################################
 
 """
     current_n = 1
