ff85db732dcdef9b8085f4ec5c18b9958ea50f4d
florienn <florienn@gmail.com>
2020-01-17 10:27:28 +0100

    Naming

--- code/algorithms/threefold.py
+++ code/algorithms/threefold.py
@@ -14,14 +14,14 @@ def three_fold(final_placement, user_input_split, current_fold, x_coordinate, y_
     """
     Determines best possible folds and coordinates for each amino (in each chunk)
     """
-    amino_info = []
+    possible_options = []
     storage_list = []
 
     for i in range(len(user_input_split)):
         chunk = user_input_split[i]
-        possible_folds, final_possible_folds, amino_info = define_folds(current_fold, x_coordinate, y_coordinate, current_amino, amino_info, storage_list, chunk)
-        possible_options = stability_score(amino_info, final_placement, chunk)
-        best_options(possible_options)
+        possible_folds, final_possible_folds, possible_options = define_folds(current_fold, x_coordinate, y_coordinate, current_amino, possible_options, storage_list, chunk)
+        possible_options_score = stability_score(possible_options, final_placement, chunk)
+        best_options(possible_options_score)
 
 
 def split_protein(user_input):
@@ -34,7 +34,7 @@ def split_protein(user_input):
     user_input_split = [user_input[i:i+3] for i in range(0, len(user_input), 3)]
     return user_input_split
 
-def define_folds(current_fold, x_coordinate, y_coordinate, current_amino, amino_info, storage_list, chunk):
+def define_folds(current_fold, x_coordinate, y_coordinate, current_amino, possible_options, storage_list, chunk):
     """
     Defines all possible folds for every move within
     """
@@ -64,10 +64,10 @@ def define_folds(current_fold, x_coordinate, y_coordinate, current_amino, amino_
         print("possible folds", possible_folds)
         possible_folds.pop(1)
 
-    amino_info = set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_amino, amino_info, chunk)
-    return possible_folds, final_possible_folds, amino_info
+    possible_options = set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_amino, possible_options, chunk)
+    return possible_folds, final_possible_folds, possible_options
 
-def set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_amino, amino_info, chunk):
+def set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_amino, possible_options, chunk):
     """
     Takes possible folds and attaches corresponding coordinates
     """
@@ -81,14 +81,10 @@ def set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_am
 
         # Simpeler maken !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (class???)
         for j in range(len(chunk)):
-            if i[j] == 1:
-                current_x += 1
-            elif i[j] == -1:
-                current_x -= 1
-            elif i[j]== 2:
-                current_y += 1
-            elif i[j] == -2:
-                current_y -= 1
+            if abs(i[j]) == 1:
+                current_x += i[j]
+            elif abs(i[j]) == 2:
+                current_y += i[j] / 2
 
             # adds a dummy fold of 0 to the last coordinates
             if j >= len(chunk) - 1:
@@ -97,23 +93,23 @@ def set_coordinates(final_possible_folds, x_coordinate, y_coordinate, current_am
                 storage_list.append([chunk[j], i[j + 1], current_x, current_y])
 
             # prevents duplicates and saves aminos with corresponding information
-            if storage_list not in amino_info:
-                amino_info.append(storage_list)
+            if storage_list not in possible_options:
+                possible_options.append(storage_list)
 
-    print("Length of amino_info: ", len(amino_info))
-    print("All possible options (amino_info): ", amino_info)
+    print("Length of possible_options: ", len(possible_options))
+    print("All possible options (possible_options): ", possible_options)
 
-    return amino_info
+    return possible_options
 
 
-def stability_score(amino_info, final_placement, chunk):
+def stability_score(possible_options, final_placement, chunk):
     """
     Calculates the stability score for every three fold
     """
 
-    possible_options = []
+    possible_options_score = []
 
-    for unit in amino_info:
+    for unit in possible_options:
         checker = True
         fold = True
         score = 0
@@ -167,24 +163,24 @@ def stability_score(amino_info, final_placement, chunk):
         # saves all possible options with corresponding stability scores
         if checker == True:
             try:
-                possible_options.append([unit[0], unit[1], unit[2], score])
+                possible_options_score.append([unit[0], unit[1], unit[2], score])
             except IndexError:
-                possible_options.append([unit[0], unit[1], score])
+                possible_options_score.append([unit[0], unit[1], score])
 
     print("////////////////")
-    print("All possible options: ", possible_options)
-    print("Length of all possible options: ", len(possible_options))
+    print("Length of all possible options: ", len(possible_options_score))
+    print("All possible options with stability score: ", possible_options_score)
 
-    return possible_options
+    return possible_options_score
 
-    # if possible_options == []:
-    #     return possible_options
-    # random_amino = random.choice(possible_options)
+    # if possible_options_score == []:
+    #     return possible_options_score
+    # random_amino = random.choice(possible_options_score)
     # current_fold = new_fold
     # if len(user_input) - 1 == len(final_placement):
         # random_amino = [user_input[len(final_placement)], 0, current_x, current_y]
 
-def best_options(possible_options):
+def best_options(possible_options_score):
     """
     Prunes possible options with lowest values
     """
@@ -193,13 +189,13 @@ def best_options(possible_options):
     best_options = []
 
     # finds lower bound
-    for value in possible_options:
+    for value in possible_options_score:
         if value[len(value) - 1] < lowest_stability_score:
             lowest_stability_score = value[len(value)- 1]
             print("Lowest stability score is: ", lowest_stability_score)
 
     # saves options with lower bound
-    for value in possible_options:
+    for value in possible_options_score:
         if value[len(value) - 1] == lowest_stability_score:
             best_options.append(value)
 
